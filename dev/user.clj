1dd748483ab73128aa56a695a3f26a9df41bf590
Luis M. Rosso <lrosso@santandertecnologia.com.ar>
2021-04-16 05:58:40 -0300

    enhancing user.clj

--- dev/user.clj
+++ dev/user.clj
@@ -11,3 +11,26 @@
 
 (defn stop []
   (mount/stop))
+
+(defn refresh []
+  (stop)
+  (tn/refresh))
+
+(defn refresh-all []
+  (stop)
+  (tn/refresh-all))
+
+(defn go
+  "starts all states defined by defstate"
+  []
+  (start)
+  :ready)
+
+(defn reset
+  "stops all states defined by defstate, reloads modified source files, and restarts the states"
+  []
+  (stop)
+  (tn/refresh :after 'user/go)
+  )
+
+(mount/in-clj-mode)
