680f67d60bc3795ebf78b0016ea732e39fd66649
Luis M. Rosso <lrosso@santandertecnologia.com.ar>
2021-04-16 05:12:27 -0300

    new interecptor set: display

--- src/accounts/web/interceptors/sets.clj
+++ src/accounts/web/interceptors/sets.clj
@@ -1,7 +1,8 @@
 (ns accounts.web.interceptors.sets
   (:require [accounts.web.interceptors.base :as base]
-            [accounts.web.interceptors.sets.account-view :as account-view])
+            [accounts.web.interceptors.sets.account-view :as account-view]))
 
 (def account-view
   (into [] (concat base/common-interceptors
-                   account-view/validate)))
+                   account-view/validate
+                   account-view/display)))
--- src/accounts/web/interceptors/sets/account-view.clj
+++ src/accounts/web/interceptors/sets/account-view.clj
@@ -1,10 +1,15 @@
 (ns accounts.web.interceptors.sets.account-view
   (:require [accounts.web.interceptors.validate :as validate]
             [accounts.web.interceptors.prepare.retrieve :as prepare-retrieve]
-            [accounts.web.interceptors.retrieve :as retrieve]))
+            [accounts.web.interceptors.retrieve :as retrieve]
+            [accounts.web.interceptors.display :as display]))
 
 (def validate
   [validate/account-id-available
    prepare-retrieve/account-for-report
    retrieve/account-detail-report
    validate/account-available-report])
+
+(def display
+  [display/entity-render
+   display/account-view])
