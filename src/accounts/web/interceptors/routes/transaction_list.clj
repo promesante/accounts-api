de5a55d4171e5130ca8a91ca18cf8573421e23e0
Luis M. Rosso <lrosso@santandertecnologia.com.ar>
2021-04-20 04:54:14 -0300

    transaction list - web: display interceptor set

--- src/accounts/web/interceptors/routes/transaction_list.clj
+++ src/accounts/web/interceptors/routes/transaction_list.clj
@@ -1,12 +1,17 @@
 (ns accounts.web.interceptors.routes.transaction-list
   (:require [accounts.web.interceptors.validate :as validate]
             [accounts.web.interceptors.prepare.retrieve :as prepare-retrieve]
-            [accounts.web.interceptors.retrieve :as retrieve]))
+            [accounts.web.interceptors.retrieve :as retrieve]
+            [accounts.web.interceptors.display :as display]))
 
 (def validate
   [validate/account-id-available
    prepare-retrieve/account-for-report
    retrieve/transactions-list])
+
+(def display
+  [display/entity-render
+   display/transactions-list])
 
 (def interceptors
   (into [] (concat validate)))
