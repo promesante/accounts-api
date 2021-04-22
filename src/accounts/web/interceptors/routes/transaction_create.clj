5cb7da0a1c612578dabf9dea7dc8cd810a02ee6a
Luis M. Rosso <lrosso@santandertecnologia.com.ar>
2021-04-21 21:27:02 -0300

    transaction create web - validate interceptor set - preparing data to retrieve accounts from datomic

--- src/accounts/web/interceptors/routes/transaction_create.clj
+++ src/accounts/web/interceptors/routes/transaction_create.clj
@@ -1,11 +1,14 @@
 (ns accounts.web.interceptors.routes.transaction-create
-  (:require [accounts.web.interceptors.validate :as validate]))
+  (:require [accounts.web.interceptors.validate :as validate]
+            [accounts.web.interceptors.prepare.retrieve :as prepare-retrieve]))
 
 (def validate
   [validate/json-params-available
    validate/json-params-structure
    validate/account-id-available
-   validate/transfer-amount])
+   validate/transfer-amount
+   prepare-retrieve/account-by-transaction
+   prepare-retrieve/transfer-account])
 
 (def interceptors
   (into [] (concat validate)))
