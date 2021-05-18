10fc7120b9480f816191c21c4b86800cf7203181
Luis M. Rosso <lrosso@santandertecnologia.com.ar>
2021-05-18 06:16:15 -0300

    rdd demo

---
+++ test/accounts/web/interceptors/validate_test.clj
@@ -0,0 +1,11 @@
+(ns accounts.web.interceptors.validate-test
+  (:require [clojure.test :refer :all]
+            [accounts.web.interceptors.validate :refer :all]))
+
+(deftest can-validate-account-id-available
+  (testing "validate account id available"
+    (let [context-1 {:request {:path-params {:account-id "account-1"}}}
+          context-2 {:request {:path-params {}}}]
+      (is (= context-1 ((:enter account-id-available) context-1)))
+      (is (= {:status 400 :body "No account id supplied as path param in URL"}
+             (:response ((:enter account-id-available) context-2)))))))
